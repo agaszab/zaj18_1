66c49e19a854c12bc5f6a496970d6a721e979d80
agaszab <gatito@poczta.pl>
2018-03-10 19:46:22 +0100

    zwierzaki

---
+++ src/main/java/pl/javastart/zaj18_1/Cat.java
@@ -0,0 +1,49 @@
+package pl.javastart.zaj18_1;
+
+public class Cat {
+   private String name;
+   private int age;
+   private String text;
+
+    public Cat(String name, int age, String text) {
+        this.name = name;
+        this.age = age;
+        this.text = text;
+    }
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/pl/javastart/zaj18_1/Zaj181Application.java
@@ -0,0 +1,12 @@
+package pl.javastart.zaj18_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zaj181Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zaj181Application.class, args);
+	}
+}
